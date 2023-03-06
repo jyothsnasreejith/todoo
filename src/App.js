254cc8441ece7e8af235f3f4f890e2c07e7b2297
jyothsnasreejith <124284566+jyothsnasreejith@users.noreply.github.com>
2023-03-06 20:26:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDo from './components/ToDo';
+
+function App() {
+  return (
+    <div className="App">
+    
+     <ToDo></ToDo>
+    </div>
+  );
+}
+
+export default App;
